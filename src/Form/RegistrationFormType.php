c68dbc84f8cc1dd8cdb27932031d11c61233af6f
rickknd1 <kendemrick@gmail.com>
2025-02-10 19:01:13 +0100

    firts
    premiere version de l'authentification sans controle de saisie coté serveur et sans centre d'interet

---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'constraints' => [
+                    new NotBlank(['message' => 'Please enter your last name']),
+                    new Length(['max' => 255])
+                ]
+            ])
+            ->add('firstname', TextType::class, [
+                'constraints' => [
+                    new NotBlank(['message' => 'Please enter your first name']),
+                    new Length(['max' => 255])
+                ]
+            ])
+            ->add('email')
+
+            ->add('gender', ChoiceType::class, [
+                'choices' => [
+                    'Male' => 'male',
+                    'Female' => 'female',
+                    'Other' => 'other'
+                ],
+                'constraints' => [
+                    new NotBlank(['message' => 'Please select your gender'])
+                ]
+            ])
+            ->add('dateOB', DateType::class, [
+                'widget' => 'choice',
+                'years' => range(date('Y') - 100, date('Y') - 13),
+                'format' => 'dd-MM-yyyy',
+                'label' => 'Date of Birth',
+                'constraints' => [
+                    new NotBlank(['message' => 'Please select your date of birth'])
+                ]
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'The password fields must match.',
+                'options' => ['attr' => ['autocomplete' => 'new-password']],
+                'required' => true,
+                'first_options' => ['label' => 'Password'],
+                'second_options' => ['label' => 'Repeat Password'],
+                'constraints' => [
+                    new NotBlank(['message' => 'Please enter a password']),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'max' => 4096,
+                    ]),
+                ],
+            ])
+
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'You should agree to our terms.',
+                    ]),
+                ],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
